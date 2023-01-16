51b443e5c1a1fd74a80f77da8e009fb7bb9f9445
KwameSei <nathanielosei92@gmail.com>
2023-01-16 15:36:58 +0000

    Gathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,22 +2,24 @@ import requests
 from sys import argv
 """Gathering information from an API"""
 
-Base_url = 'https://jsonplaceholder.typicode.com'
-# Getting user by putting user id from the command line
-user = requests.get(f'{Base_url}/users/{argv[1]}').json()
-todos = requests.get(f'{Base_url}/todos', params={'userId': argv[1]}).json()
+if __name__ == '__main__':
+    Base_url = 'https://jsonplaceholder.typicode.com'
+    # Getting user by putting user id from the command line
+    user = requests.get(f'{Base_url}/users/{argv[1]}').json()
+    todos = requests.get(f'\
+            {Base_url}/todos', params={'userId': argv[1]}).json()
 
-total_tasks = len(todos)
-completed = 0
-completed_tasks = []
+    total_tasks = len(todos)
+    completed = 0
+    completed_tasks = []
 
-for todo in todos:
-    if todo['completed']:
-        completed += 1
-        completed_tasks.append(todo["title"])
-output = f'Employee {user["name"]} is done with tasks \
-        ({completed} / {total_tasks}): '
+    for todo in todos:
+        if todo['completed']:
+            completed += 1
+            completed_tasks.append(todo["title"])
+    output = f'Employee {user["name"]} is done with tasks \
+            ({completed}/{total_tasks}): '
 
-print(output)
-for task in completed_tasks:
-    print(f'\t {task}')
+    print(output)
+    for task in completed_tasks:
+        print(f'\t {task}')
