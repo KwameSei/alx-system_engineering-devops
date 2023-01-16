33f4516d664efda7343e08ef9d99ae0532853558
KwameSei <nathanielosei92@gmail.com>
2023-01-16 14:01:35 +0000

    Gathering data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+import requests
+from sys import argv
+
+Base_url = 'https://jsonplaceholder.typicode.com'
+# Getting user by putting user id from the command line
+user = requests.get(f'{Base_url}/users/{argv[1]}').json()
+todos = requests.get(f'{Base_url}/todos', params = {'userId' : argv[1]}).json()
+
+total_tasks = len(todos)
+#print('Total requests: '.format(total_tasks))
+completed = 0
+completed_tasks = []
+
+for todo in todos:
+    if todo['completed']:
+        completed += 1
+        completed_tasks.append(todo["title"])
+output = f'Employee {user["name"]} is done with tasks ({completed}/{total_tasks}):'
+
+print(output)
+for task in completed_tasks:
+    print(f'\t {task}')
