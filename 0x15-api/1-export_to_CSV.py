93e13c8bf17669d20b4fb5fb4e3c1649121f2d48
KwameSei <nathanielosei92@gmail.com>
2023-01-17 04:35:54 +0000

    Exporting to CSV file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Gathering information from an API
+"""
+import csv
+import requests
+from sys import argv
+
+
+Base_url = 'https://jsonplaceholder.typicode.com'
+"""REST API url"""
+
+
+if __name__ == "__main__":
+    user = requests.get('{}/users/{}'.format(Base_url, argv[1])).json()
+    todos = requests.get('\
+            {}/todos'.format(Base_url), params={'userId': argv[1]}).json()
+
+    with open('{}.csv'.format(argv[1]), 'w', encoding='utf8') as f:
+
+        writer = csv.writer(f, delimiter=',', quotechar='\
+                "', quoting=csv.QUOTE_ALL)
+
+        for todo in todos:
+            writer.writerow(['{}'.format(argv[1]), '{}'.format(user['name']),
+                            '{}'.format(str(todo['completed'])),
+                             '{}'.format(todo['title'])])
